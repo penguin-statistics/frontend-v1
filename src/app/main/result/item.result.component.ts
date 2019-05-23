9a08f4229c7304752498a961564c2e69a00f23fe
AlvISsReimu <alvissreimu@gmail.com>
2019-05-23 16:55:44 -0700

    add Now Loading for result pages

--- src/app/main/result/item.result.component.ts
+++ src/app/main/result/item.result.component.ts
@@ -12,6 +12,7 @@ export class ItemResultComponent implements OnInit {
 
     itemList: any = [];
     itemResult: any = null;
+    isLoading: boolean = false;
 
     constructor(private http: HttpClient, public penguinService: PenguinService, public selectedService: SelectedService) { }
 
@@ -31,16 +32,19 @@ export class ItemResultComponent implements OnInit {
 
     private _refreshItemResult() {
         if (this.selectedService.selections.result_by_item.selectedItem != null && this.selectedService.selections.result_by_item.selectedItem.id != null) {
+            this.isLoading = true;
             this.http.get("/PenguinStats/api/result/item/" + this.selectedService.selections.result_by_item.selectedItem.id)
                 .subscribe(
                     (val) => {
                         this.itemResult = val;
                         this._sortItemResult();
+                        this.isLoading = false;
                     },
                     error => {
                         alert('获取结果失败。\n' + error.message + "\n如果可以的话希望能将以上信息提供给作者，谢谢！");
                         // this.itemResult = {"item":{"img":"http://p3.qhimg.com/dr/100__/t018a302bcf4e1fb3c1.png","itemType":"material","name":"聚酸酯组","id":10,"materialCategory":"ester","rarity":2},"drops":[{"times":46,"quantity":1,"stage":{"specialDrop":[23],"extraDrop":[8,9,10,20,21,22,28,30],"code":"4-5","stageType":"normal","normalDrop":[22],"id":4,"apCost":18}},{"times":48,"quantity":1,"stage":{"specialDrop":[31],"extraDrop":[8,9,10,20,21,22,28,30],"code":"4-9","stageType":"normal","normalDrop":[30],"id":8,"apCost":21}},{"times":5,"quantity":1,"stage":{"specialDrop":[11],"extraDrop":[8,9,10,20,21,22,28,30],"code":"3-8","stageType":"normal","normalDrop":[10],"id":0,"apCost":18}}]}
                         // this._sortItemResult();
+                        this.isLoading = false;
                     },
                     () => {
                     });
--- src/app/main/result/stage.result.component.ts
+++ src/app/main/result/stage.result.component.ts
@@ -13,6 +13,7 @@ export class StageResultComponent implements OnInit {
   stageList: any = [];
   chapterList: Chapter[];
   stageResult: any = null;
+  isLoading: boolean = false;
 
   constructor(private http: HttpClient, public penguinService: PenguinService, public selectedService: SelectedService) { }
 
@@ -78,16 +79,19 @@ export class StageResultComponent implements OnInit {
   private _refreshStageResult() {
     if (this.selectedService.selections.result_by_stage.selectedStage != null && this.selectedService.selections.result_by_stage.selectedStage.stageID != null && this.selectedService.selections.result_by_stage.stageType != null) {
       this.stageResult = null;
+      this.isLoading = true;
       this.http.get("/PenguinStats/api/result/stage/" + this.selectedService.selections.result_by_stage.selectedStage.stageID + "/" + this.selectedService.selections.result_by_stage.stageType)
         .subscribe(
           (val) => {
             this.stageResult = val;
             this._sortStageResult();
+            this.isLoading = false;
           },
           error => {
             alert('获取结果失败。\n' + error.message + "\n如果可以的话希望能将以上信息提供给作者，谢谢！");
             // this.stageResult = { "times": 57, "drops": [{ "item": { "img": "http://p0.qhimg.com/dr/100__/t01ad2e7fb00256fd86.png", "itemType": "material", "name": "源岩", "id": 0, "materialCategory": "rock", "rarity": 0 }, "quantity": 18 }, { "item": { "itemType": "furniture", "name": "家具", "id": -1, "rarity": -1 }, "quantity": 1 }, { "item": { "img": "http://p7.qhimg.com/dr/100__/t0199099c5b08a71a7c.png", "itemType": "material", "name": "固源岩", "id": 1, "materialCategory": "rock", "rarity": 1 }, "quantity": 15 }, { "item": { "img": "http://p0.qhimg.com/dr/100__/t01e388e4bd786583cc.png", "itemType": "material", "name": "固源岩组", "id": 2, "materialCategory": "rock", "rarity": 2 }, "quantity": 24 }, { "item": { "img": "http://p1.qhimg.com/dr/100__/t0155cbcc61b719b8f0.png", "itemType": "material", "name": "提纯源岩", "id": 3, "materialCategory": "rock", "rarity": 3 }, "quantity": 3 }, { "item": { "img": "http://p0.qhimg.com/dr/100__/t01d215deb8b5d92450.png", "itemType": "material", "name": "破损装置", "id": 4, "materialCategory": "device", "rarity": 0 }, "quantity": 6 }, { "item": { "img": "http://p7.qhimg.com/dr/100__/t0168ce9414da96fd12.png", "itemType": "material", "name": "装置", "id": 5, "materialCategory": "device", "rarity": 1 }, "quantity": 9 }, { "item": { "img": "http://p5.qhimg.com/dr/100__/t011e4890ca178623a8.png", "itemType": "material", "name": "全新装置", "id": 6, "materialCategory": "device", "rarity": 2 }, "quantity": 2 }, { "item": { "img": "http://p5.qhimg.com/dr/100__/t012969254d8c583793.png", "itemType": "material", "name": "扭转醇", "id": 24, "materialCategory": "alcohol", "rarity": 2 }, "quantity": 1 }], "stage": { "specialDrop": [3], "extraDrop": [0, 1, 2, 4, 5, 6, 24], "code": "4-6", "normalDrop": [2], "id": 5, "apCost": 18 } };
             // this._sortStageResult();
+            this.isLoading = false;
           },
           () => {
           });
