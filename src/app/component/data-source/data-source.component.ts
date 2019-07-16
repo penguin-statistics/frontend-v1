83c6d55b8f5f9f3915723ebb4990618bad5c02db
AlvISsReimu <alvissreimu@gmail.com>
2019-07-15 18:59:07 -0700

    When choosing "Personal" in data source selector, do not check localStorage any more, will check userID instead from cookies.

--- src/app/component/data-source/data-source.component.ts
+++ src/app/component/data-source/data-source.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { PenguinService } from 'src/app/service/penguin.service';
 import { MatSnackBar } from '@angular/material';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-data-source',
@@ -14,7 +15,8 @@ export class DataSourceComponent implements OnInit {
 
   constructor(
     public penguinService: PenguinService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private cookieService: CookieService
   ) { }
 
   ngOnInit() {
@@ -24,20 +26,13 @@ export class DataSourceComponent implements OnInit {
     if (this.penguinService.isPersonal === isPersonal) {
       return;
     }
-    if (!window.localStorage) {
-      this._snackBar.open(
-        "您的浏览器暂不支持本地数据，请升级或者换浏览器再试。", "x", { duration: 2000 }
-      );
-      return;
-    }
-    let localStageTimesStr = localStorage.getItem("stageTimes");
-    let localDropMatrixStr = localStorage.getItem("dropMatrix");
-    if (isPersonal && (!localStageTimesStr || !localDropMatrixStr)) {
-      this._snackBar.open("您当前还未上传过掉落数据。", "x", { duration: 2000 });
-      return;
-    }
     if (isPersonal) {
-      this._snackBar.open("由于即将更新本地上传管理功能，此处的本地数据可以正常显示，但暂时不会更新。如有不便，敬请谅解。", "x", { duration: 4000 });
+      const userID: string = this.cookieService.get("userID");
+      if (!userID) {
+        this._snackBar.open(
+          "您当前未登录，无法获取完整个人上传数据。", "x", { duration: 3000 }
+        );
+      }
     }
     this.penguinService.isPersonal = isPersonal;
     this.refreshResult.emit();
